af1ce6f01ac14aa53bbca03462ebe411866abc87
Ewan Burnett <EwanBurnettSK@outlook.com>
2023-08-08 14:32:28 +0100

    Library Configuration
    Dewdrop now can be built as either a Static or a Shared library using
    CMake 3.8
    A Development Project was also added.

---
+++ Examples/00-DEV/main.cpp
@@ -0,0 +1,5 @@
+#include <Dewdrop.h>
+
+int main() {
+    Dewdrop::Init(); 
+}
---
+++ src/Dewdrop.cpp
@@ -0,0 +1,6 @@
+#include "../include/Dewdrop.h"
+#include <cstdio>
+
+void Dewdrop::Init() {
+    printf("Initialising Dewdrop %d.%d.%d.%d\n", DEWDROP_VERSION_MAJOR, DEWDROP_VERSION_MINOR, DEWDROP_VERSION_PATCH, DEWDROP_VERSION_BUILD);
+}
