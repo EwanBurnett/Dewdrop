f7a2206be2b48128c5959bbfa499cf61913eb696
Ewan Burnett <EwanBurnettSK@outlook.com>
2023-08-08 15:55:12 +0100

    Third Party Dependencies: IMGUI GLFW
    Added dependencies to IMGUI and GLFW, via git submodules and
    fetch_content.

--- Examples/00-DEV/main.cpp
+++ Examples/00-DEV/main.cpp
@@ -1,5 +1,21 @@
 #include <Dewdrop.h>
+#include <glfw/glfw3.h> 
+#include <imgui.h>
+#include <cstdio>
 
 int main() {
     Dewdrop::Init(); 
+
+    glfwInit();
+
+    char title[256];
+    sprintf(title, "Dewdrop-DEV v%d.%d.%d.%d", Dewdrop::DEWDROP_VERSION_MAJOR, Dewdrop::DEWDROP_VERSION_MINOR, Dewdrop::DEWDROP_VERSION_PATCH, Dewdrop::DEWDROP_VERSION_BUILD);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, title, nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents(); 
+    }
+
+    glfwDestroyWindow(window);
 }
